311fe6689292fe305d16dc71f26e742c0fc2fa6b
HelloCodeMing <huanmingwong@163.com>
2015-01-01 15:16:36 +0800

    add default IDF

--- src/main/scala/com/wanghuanming/TFIDF/TFIDF.scala
+++ src/main/scala/com/wanghuanming/TFIDF/TFIDF.scala
@@ -10,9 +10,10 @@ import org.ansj.splitWord.analysis.ToAnalysis
 object TFIDF {
   val pathPrefix = "src/main/resources/"
   val stopwords = List("engStopwords.txt", "zhStopwords.txt").flatMap(file => Source.fromFile(pathPrefix + file).getLines.map(_.trim))
+  var defaultIDF = 0.0
 
   /**
-    * return : List[String, Double] = List(word, freq)
+    * return : List[(String, Double)] = List(word, freq)
     */
 
   def getKeywords(content: String, topN: Int = 10, corpus: String) = {
@@ -21,19 +22,22 @@ object TFIDF {
     val idf = IDF(corpus)
     val tfidf = tf.map { item =>
       // word -> frequency
-      item._1 -> item._2 * idf.getOrElse(item._1, 8.8)
+      item._1 -> item._2 * idf.getOrElse(item._1, defaultIDF)
     }.toList
     tfidf.sortBy(_._2).reverse.take(topN)
   }
 
   def IDF(corpus: String): Map[String, Double] = {
     if (new File(pathPrefix + "IDF.cache").exists) {
-      return Source.fromFile(pathPrefix + "IDF.cache").getLines.map(_.split(" ")).filter(_.length == 2)
+      val res = Source.fromFile(pathPrefix + "IDF.cache").getLines.map(_.split(" ")).filter(_.length == 2)
         .map { item => item(0) -> item(1).toDouble }.toMap
+      defaultIDF = Math.log(res.size)
+      return res
     }
     val files = new File(corpus).listFiles
-    val fileCnt = files.length
+    val fileCnt = files.size
     val writer = new PrintWriter(pathPrefix + "IDF.cache")
+    defaultIDF = Math.log(fileCnt)
     val res = files.map { file =>
       ToAnalysis.parse(Source.fromFile(file).getLines.reduce(_ + _))
         .toArray.map(_.toString.split("/")(0)).toList.distinct
